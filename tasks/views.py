278f25768a9b76a1f3599890b32956003250b00d
rockband_666 <rockband_666@hotmail.com>
2024-05-08 00:08:05 -0400

    mi conexion de usuario con pasword

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 # Create your views here.
 
-def helloworld(request):
-    return HttpResponse('Hello world')
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1']== request.POST['password2']:
+            # register user
+            try:
+                user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])
+                user.save()
+                return HttpResponse('User created successfully')
+            except:
+                return HttpResponse('Username already exists')
+            
+        return HttpResponse('password do not match')
