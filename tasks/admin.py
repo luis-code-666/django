95fd576e0e61c2d0c044044dd0037acf4317319b
rockband_666 <rockband_666@hotmail.com>
2024-05-26 22:38:26 -0400

    mi commit que me olvide hacer despues de tiempo

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Task
+
 class TaskAdmin(admin.ModelAdmin):
     readonly_fields = ("created",)
     
--- tasks/models.py
+++ tasks/models.py
@@ -9,5 +9,6 @@ class Task(models.Model):
     datecompleted = models.DateTimeField(null=True)
     importan = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.title + ' - by ' + self.user.username
